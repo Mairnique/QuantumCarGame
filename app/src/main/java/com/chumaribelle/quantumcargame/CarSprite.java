2fc296bb714dcc820db203c8a677732b3022fa91
2024kthyagar <karthik6002@gmail.com>
2022-12-07 11:16:22 -0500

    Made car sprite and added stuff to game view

--- app/src/main/java/com/chumaribelle/quantumcargame/CarSprite.java
+++ app/src/main/java/com/chumaribelle/quantumcargame/CarSprite.java
@@ -1,4 +1,28 @@
 package com.chumaribelle.quantumcargame;
 
-public class CarSprite {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+public class CarSprite extends RectF {
+    int dX, dY;
+    int left, top, right, bottom;
+    Bitmap carImg;
+
+    public CarSprite(int l, int t, int r, int b, int dX, int dY, Bitmap bitmap) {
+        super(l,t,r,b);
+        carImg = bitmap;
+        this.dX = dX;
+        this.dY = dY;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(carImg, null, this, new Paint());
+    }
+
+    public void update() {
+        offset(dX, dY);
+    }
 }
--- app/src/main/java/com/chumaribelle/quantumcargame/MainActivity.java
+++ app/src/main/java/com/chumaribelle/quantumcargame/MainActivity.java
@@ -2,14 +2,19 @@ package com.chumaribelle.quantumcargame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GameView mGameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        mGameView = new GameView(this);
+        mGameView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(mGameView);
     }
 }
