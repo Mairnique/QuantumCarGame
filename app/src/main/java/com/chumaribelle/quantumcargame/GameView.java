5c229a9fd9612840495891dd0c42e838e5528b25
karthikt <karthik6002@gmail.com>
2022-12-26 11:54:59 -0500

    Drew the background and resized car

--- app/src/main/java/com/chumaribelle/quantumcargame/GameView.java
+++ app/src/main/java/com/chumaribelle/quantumcargame/GameView.java
@@ -5,6 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -18,6 +21,8 @@ public class GameView extends SurfaceView implements Runnable{
     private CarSprite mCar;
     private boolean mRunning;
     private Thread mGameThread;
+    private Paint mPaint;
+    private Path mPath;
 
     public GameView(Context context) {
         super(context);
@@ -38,7 +43,7 @@ public class GameView extends SurfaceView implements Runnable{
     public void init(Context context) {
         mContext = context;
         mSurfaceHolder = getHolder();
-
+        mPaint = new Paint();
     }
 
     @Override
@@ -47,7 +52,7 @@ public class GameView extends SurfaceView implements Runnable{
         mViewWidth = w;
         mViewHeight = h;
         Bitmap carBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.racecar);
-        carBitmap = Bitmap.createScaledBitmap(carBitmap, (int) (mViewWidth/6.5), mViewHeight/8, false);
+        carBitmap = Bitmap.createScaledBitmap(carBitmap, (int) (mViewWidth/8.5), (int) (mViewHeight/10), false);
         mCar = new CarSprite(20, (mViewHeight/2)-(carBitmap.getHeight()/2), 20+carBitmap.getWidth(),
                 (mViewHeight/2)+(carBitmap.getHeight()/2), 0, 0, carBitmap);
 
@@ -75,11 +80,36 @@ public class GameView extends SurfaceView implements Runnable{
             if (mSurfaceHolder.getSurface().isValid()) {
                 canvas = mSurfaceHolder.lockCanvas();
                 canvas.save();
-                canvas.drawColor(Color.WHITE);
+
+                drawBackground(canvas);
+
                 mCar.draw(canvas);
 
                 canvas.restore();
                 mSurfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    public void drawBackground(Canvas canvas) {
+        // Grass
+        mPaint.setColor(Color.GREEN);
+        canvas.drawRect(0,0,mViewWidth,(float) mViewHeight/7, mPaint);
+        canvas.drawRect(0, (float) mViewHeight*6/7, mViewWidth, mViewHeight, mPaint);
+
+        // Road
+        mPaint.setColor(Color.GRAY);
+        canvas.drawRect(0, (float) mViewHeight/7, mViewWidth, (float) mViewHeight*6/7, mPaint);
+
+        // Lane Markers
+        mPaint.setColor(Color.WHITE);
+        for(int i=2; i<6; i++) {
+            float top = (float) mViewHeight/7*i-5;
+            float bottom = top+10;
+            for(int j=0; j<=10; j++) {
+                float left = (float) mViewWidth/10*j + (float) mViewWidth/20;
+                float right = left+25;
+                canvas.drawRect(left, top, right, bottom, mPaint);
             }
         }
     }
